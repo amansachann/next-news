2fe58568185f2affd7a046154a8a83cb7763b7d5
Aman Sachan <codewithaman78@gmail.com>
2025-01-13 20:23:03 +0530

    add: news, news details pages

---
+++ src/app/news/[slug]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewsNotFoundPage = () => {
+  return (
+    <div id="error">
+      <h1>Article not found!</h1>
+      <p>Unfortunately, the article you are looking for does not exist. </p>
+    </div>
+  );
+};
+
+export default NewsNotFoundPage;
