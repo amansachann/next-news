204aedf910ec864121973081f79711bfeb96ae45
Aman Sachan <codewithaman78@gmail.com>
2025-01-15 00:03:24 +0530

    fix: add server side data fetching insted of client side data fetching

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,40 +1,18 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 // import { DUMMY_NEWS } from "../../../../dummy-news";
 import NewsList from "@/components/news-list";
 
-const NewsPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [news, setNews] = useState([]);
-  useEffect(() => {
-    setIsLoading(true);
-    async function fetchNews() {
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setIsLoading(false);
-      }
-      const data = await response.json();
-      setNews(data);
-      setIsLoading(false);
-    }
-    fetchNews();
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
-  if (error) {
-    return <p>{error}</p>;
-  }
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 };
